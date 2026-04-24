fa55dee72b5de79f1a0b47e8c049ac2b7276d514
anticristo2009dx <driverocarrasco@gmail.com>
2026-04-23 19:51:18 -0500

    beta 0.0.4
    hubo una gran mejoria despues de la beta 0.0.3 siendo esta la correccion de multiples errores, un login funcional y conexion a base de datos real

---
+++ control.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!empty ($_POST["btn"])) {
+    if (empty($_POST["user"])) and (empty($_POST["password"])) {
+        echo "Por favor ingrese sus datos";
+    } else {
+        # code...
+    }
+    
+}
+
+?>
